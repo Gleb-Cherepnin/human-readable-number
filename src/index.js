19cee2678e4a4ff0d53452ae7baa272b3f33d16d
Glebasik58 <gglebasemil@gmail.com>
2025-07-21 07:25:33 +0500

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  const teens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (number === 0) return 'zero';
+
+  let result = '';
+
+  const hundred = Math.floor(number / 100);
+  const tenUnits = number % 100;
+  const ten = Math.floor(tenUnits / 10);
+  const unit = tenUnits % 10;
+
+  if (hundred > 0) {
+    result += `${ones[hundred]} hundred`;
+    if (tenUnits > 0) result += ' ';
+  }
+
+  if (tenUnits > 0) {
+    if (tenUnits < 10) {
+      result += ones[tenUnits];
+    } else if (tenUnits < 20) {
+      result += teens[tenUnits - 10];
+    } else {
+      result += tens[ten];
+      if (unit > 0) result += ` ${ones[unit]}`;
+    }
+  }
+
+  return result;
 };
